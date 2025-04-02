50d0984a4d5a3a1c57bc6e71ee39339e17284e0a
Manreet Thind <mthind2315@conestogac.on.ca>
2025-04-02 11:01:13 -0400

    Added First DOM Manipulation

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,1 +1,16 @@
 console.log('hello world')
+
+const helloWolrdBox = document.getElementById('hello-world')
+
+helloWolrdBox.innerHTML = 'hello <b>world</b>'
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+    console.log('success', response)
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
