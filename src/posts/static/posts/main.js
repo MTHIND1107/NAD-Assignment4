ff4cc48c853dfca36632f234456e25a0901c8597
Manreet Thind <mthind2315@conestogac.on.ca>
2025-04-03 17:41:59 -0400

    Getting Posts Objects with AJAX

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,6 +1,7 @@
 console.log('hello world')
 
 const helloWolrdBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
 
 $.ajax({
     type: 'GET',
@@ -19,8 +20,13 @@ $.ajax({
     url: '/data/',
     success: function(response){
     console.log(response)
-    const data = JSON.parse(response.data)
+    const data = response.data
     console.log(data)
+    data.forEach(el => {
+        postsBox.innerHTML += `
+          ${el.title} - <b>${el.body}</b><br>
+        `
+    });
     },
     error: function(error){
         console.log(error)
