a9b6720b98e882ced91ecdc2198c171b56adb462
Manreet Thind <mthind2315@conestogac.on.ca>
2025-04-06 17:15:25 -0400

    success & error messages displaying successfully

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -9,6 +9,8 @@ const postForm = document.getElementById('post-form')
 const title = document.getElementById('id_title')
 const body = document.getElementById('id_body')
 const csrf = document.getElementsByName('csrfmiddlewaretoken')
+const alertBox = document.getElementById('alert-box')
+
 console.log('csrf', csrf[0].value)
 
 const getCookie = (name) => {
@@ -126,9 +128,33 @@ postForm.addEventListener('submit', e=>{
         },
         success: function(response) {
         console.log(response)
+        postsBox.insertAdjacentHTML('afterbegin', `
+                <div class="card mb-2">
+                     <div class="card-body">
+                       <h5 class="card-title">${response.title}</h5>
+                       <p class="card-text">${response.body}</p>
+                     </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class="col-2">
+                                    <a href="#" class="btn btn-primary">Details</a>
+                                </div>
+                                <div class="col-2">
+                                    <form class="like-unlike-forms" data-form-id="${response.id}">
+                                        <button href="#" class="btn btn-primary" id="like-unlike-${response.id}">Like (0)</button>
+                                    </form>
+                                </div>
+                            </div>
+                       </div>
+                   </div>       
+            `)
+            likeUnlikePosts()
+            $('#addPostModal').modal('hide')
+            handleAlerts('success', 'New Post Added!')
         },
         error: function(error) {
-        console.log(error)
+            console.log(error)
+            handleAlerts('danger', 'OOPS...Something went wrong')
         }
     })
 })
